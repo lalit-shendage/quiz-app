c88c184ac4db1577abc15cd0dd01fce2df30479e
lalit-shendage <shendage.lalit@gmail.com>
2023-12-23 12:35:35 +0530

    mongo connection update

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ user=process.env.mongoURI
 
 module.exports = async () => {
   try {
-    await mongoose.connect(`${user}`);
+    await mongoose.connect(`mongodb+srv://${user}@cluster0.dwnwv8t.mongodb.net`);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
