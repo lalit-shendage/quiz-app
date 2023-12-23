94554ac64ffcef6d56d92a28dc1d8ad10f0d06d9
lalit-shendage <shendage.lalit@gmail.com>
2023-12-23 12:32:01 +0530

    mongo uri change

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ user=process.env.mongoURI
 
 module.exports = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://${user}@cluster0.dwnwv8t.mongodb.net/quiz_app`);
+    await mongoose.connect(`${user}`);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
